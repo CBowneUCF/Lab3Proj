784db43e9ccd7e6147d83507996465ea3e2cb787
CBowneCPiedmont <78770940+CBowneCPiedmont@users.noreply.github.com>
2024-01-25 18:25:55 -0500

    Script1CleanupAndScript3CreationCommit

--- Assets/LabScript_1.cs
+++ Assets/LabScript_1.cs
@@ -1,73 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+
+// Intermediate Game Development Lab 3 Sub-Assignment 1.
+//Written by Carl Bowne and John(? Place full name here please.)
 
 public class LabScript_1 : MonoBehaviour
 {
-    public string courseName;
+    public string courseName = null;
 
-    //Assume everything is based on a 16 week course schedule with no breaks
-    //Does not account for floating point, as scale is not large enough to matter
-
+    //Assumption: Course schedule is assumed to be a full 16 week course schedule with no breaks.
     const int COURSELENGTH = 16; //Maximum weeks in a course
 
+    //Assumption: "Difficulty" is measured against a maximum of 16, 1 of each item for every week.
     [Range(0, COURSELENGTH)]public int moduleCount;
     [Range(0, COURSELENGTH)]public int readingMatCount;
     [Range(0, COURSELENGTH)]public int assignmentCount;
     [Range(0, COURSELENGTH)]public int quizCount;
-    public bool hasIntructorTaughtCourse;
+    public bool hasIntructorTaughtCourse; //The instructor having taught the class before is treated as making it easier for the students.
 
-    private float resultTotal = 0f;
-
-
-
-    
+    void Start() => Main();
 
 
-
-    // 12/16 = x/30
-
-    //
-    // 15/100 = 
-
-
-
-
-    
-    void Start()
+    void Main()
     {
-        int instructorXpValue = 10;
-
-        if(CheckAllZeros()) {
-            Debug.LogError("This course is empty. Add something.");
+        //Error Checks.
+        if(courseName == "")
+        {
+            Debug.LogError("This course is missing a name. Please add a name.");
             return;
         }
-        if(hasIntructorTaughtCourse)
+        if (CheckAllZeros())
         {
-           instructorXpValue = 0;
+            Debug.LogError("This course has nothing in it. Please add content.");
+            return;
         }
-        CalculateFactorPercentage(moduleCount, 15);
-        CalculateFactorPercentage(readingMatCount, 30);
-        CalculateFactorPercentage(quizCount, 15);
-        CalculateFactorPercentage(assignmentCount, 30);
 
-        Debug.LogFormat("Challenge Rating for course {0} is {1}", courseName, ((resultTotal+instructorXpValue)/10));
+        float resultTotal = 0f;
+        resultTotal += CalculateFactorPercentage(moduleCount, 15);
+        resultTotal += CalculateFactorPercentage(readingMatCount, 30);
+        resultTotal += CalculateFactorPercentage(quizCount, 15);
+        resultTotal += CalculateFactorPercentage(assignmentCount, 30);
+        resultTotal += hasIntructorTaughtCourse ? 0f : 10f;
 
+        int finalResult = (int)Mathf.Lerp(1, 10, resultTotal/100);
+
+        Debug.LogFormat("Challenge Rating for course {0} is {1}", courseName, finalResult);
     }
 
-    void CalculateFactorPercentage(int amountOfFactor, int factorPercentage)
-    {
-        resultTotal += ((float)amountOfFactor * (float)factorPercentage) / (float)COURSELENGTH;
-        Debug.Log(resultTotal);
-    }
 
-    bool CheckAllZeros()
-    {
-        return moduleCount == 0 && assignmentCount == 0 && readingMatCount == 0 && quizCount == 0 && !hasIntructorTaughtCourse;
-    }
-    void CalcuateChallengeScore()
-    {
-        
-    }
+    float CalculateFactorPercentage(int amountOfFactor, int factorPercentage) =>
+        ((float)amountOfFactor * (float)factorPercentage) / (float)COURSELENGTH;
 
+    bool CheckAllZeros() =>
+        moduleCount == 0 && assignmentCount == 0 && readingMatCount == 0 && quizCount == 0;
 }
